8804a900a193f3d914540b7c32c71fffdf845e0c
Nikita-ysov <156422968+Nikita-ysov@users.noreply.github.com>
2025-05-21 14:22:36 +0500

    Add files via upload

--- вебМедведь/script/script.js
+++ вебМедведь/script/script.js
@@ -1,32 +1,24 @@
-// слайдер
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
 
-let currentIndex = 0;
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
-function showSlides(index) {
-   const slides = document.querySelectorAll('.slide');
-   const dots = document.querySelectorAll('.dot');
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
 
-   if (index >= slides.length) currentIndex = 0; // Если индекс больше количества слайдов
-   if (index < 0) currentIndex = slides.length - 1; // Если индекс меньше нуля
 
-   slides.forEach((slide, i) => {
-       slide.style.display = (i === currentIndex) ? 'block' : 'none'; // Показываем текущий слайд
-       dots[i].className = dots[i].className.replace(' active', ''); // Убираем активный класс у всех индикаторов
-   });
+});
 
-   dots[currentIndex].className += ' active'; // Добавляем активный класс к текущему индикатору
-}
 
-function changeSlide(n) {
-   showSlides(currentIndex += n);
-}
 
-function currentSlide(n) {
-   showSlides(currentIndex = n);
-}
-
-// Инициализация первого слайда
-showSlides(currentIndex);
 // форма 
 
 document.getElementById('firstForm').addEventListener('submit', function(event) {
